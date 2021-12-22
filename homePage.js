321f2597e240e8e325b524164e8b368f02551793
LaShell-Young <93954809+LaShell-Young@users.noreply.github.com>
2021-12-21 20:44:18 -0600

    Add files via upload

---
+++ homePage.js
@@ -0,0 +1,11 @@
+﻿const images = ['./images/lashell2021Grad.JPG', './images/lashell-pic2.PNG', './images/lashell-pic3.PNG'];
+var index = 1;
+
+function changeImg() {
+    //get element by id and change image src
+    document.getElementById('headerImg').src = images[index];
+    index++;
+    if (index >= images.length) {
+        index = 0;
+    }
+}
